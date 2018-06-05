19f45d7452664e26ac641d31a44d3b20b077f9e8
Pol <polbene95@gmail.com>
2018-06-05 13:33:48 +0200

    Dinamic Background And Responsiveness (3 views)

--- script/main-js.js
+++ script/main-js.js
@@ -1,28 +1,29 @@
 $(".not-waiting-div").hide();
 $(".waiting-div").show();
 document.getElementById("search-bar").onkeydown = function () {
-            if (event.keyCode == 13) {
-                getCity();
-            };
-        }
+    if (event.keyCode == 13) {
+        getCity();
+    };
+}
+
 function getCity() {
 
     var getCity = $("#search-bar").val();
     $.ajax('https://api.openweathermap.org/data/2.5/forecast?q=' + getCity + '&APPID=013e94b7065991843e6435dd41005e59').done(function (json) {
         data = json;
         console.log(data);
+        backgroundSetter();
         printInfo();
         $(".not-waiting-div").show();
         $(".waiting-div").hide();
-        
+        $("#search-bar").text("");
+
     }).fail(function () {
         $(".not-waiting-div").hide();
         $(".waiting-div p").text(getCity + " not found");
         $(".waiting-div").show();
 
     });
-    
-    $("#search-bar").text("");
 }
 
 function printInfo() {
@@ -32,17 +33,22 @@ function printInfo() {
 
     var description = list[0].weather[0].description;
     var humidity = list[0].main.humidity;
+    var weather = list[0].weather[0].main;
 
     var tempNow = list[0].main.temp - 273.15;
     var tempMax = list[0].main.temp_max - 273.15;
     var tempMin = list[0].main.temp_min - 273.15;
 
+    //    var icon = createIcon(weather);
+    //    icon.classList.add("main-weather-icon");
+
     var tempNowC = tempNow.toString().split(".")[0];
     var tempMaxC = tempMax.toString().split(".")[0];
     var tempMinC = tempMin.toString().split(".")[0];
 
-    $(".degress h1").text(tempNowC + " º")
-    $(".city-name h2").text(cityName)
+    $(".degress h1").text(tempNowC + " º");
+    //    $(".degress").append(icon);
+    $(".city-name h2").text(cityName);
     $(".city-name h4").text(description + " (" + tempMaxC + "º / " + tempMinC + "º )");
     $(".city-name div p").text(humidity);
 
@@ -52,16 +58,30 @@ function printInfo() {
 function extraInfo() {
     var list = data.list;
     var extraInfo = document.getElementById("extra-info");
-    extraInfo.innerHTML="";
+    extraInfo.innerHTML = "";
     for (let i = 0; i < 9; i++) {
 
         var mainDiv = document.createElement("div");
         $(mainDiv).addClass("every-time");
+
         var dateDiv = document.createElement("div");
+        dateDiv.classList.add("every-time-div");
+        dateDiv.classList.add("hide-in-mobil");
+
         var iconDiv = document.createElement("div");
+        iconDiv.classList.add("every-time-div");
+
         var tempDiv = document.createElement("div");
+        tempDiv.classList.add("every-time-div");
+        tempDiv.classList.add("hide-in-mobil");
+
         var maxMinDiv = document.createElement("div");
+        maxMinDiv.classList.add("hide-in-mobil");
+        maxMinDiv.classList.add("every-time-div");
+
         var windDiv = document.createElement("div");
+        windDiv.classList.add("hide-in-mobil");
+        windDiv.classList.add("every-time-div");
 
         var date = list[i].dt_txt;
         var splitDate = date.split(" ")[1];
@@ -77,19 +97,7 @@ function extraInfo() {
 
         var wind = list[i].wind.speed;
 
-        var icon = document.createElement("i");
-        if (weather == "Rain") {
-            $(icon).addClass("wi wi-rain")
-        }
-        if (weather == "Clouds") {
-            $(icon).addClass("wi wi-cloud")
-        }
-        if (weather == "Snow") {
-            $(icon).addClass("wi wi-snow")
-        }
-        if (weather == "Clear") {
-            $(icon).addClass("wi wi-day-sunny")
-        }
+        var icon = createIcon(weather);
 
         dateDiv.innerHTML = splitDate;
         iconDiv.append(icon);
@@ -109,14 +117,36 @@ function extraInfo() {
 
 function backgroundSetter() {
     var list = data.list;
-    var date = list[0].dt_txt;
-    var dateSplit = date.split(" ")[1];
-    var timeDate = parseFloat(dateSplit);
-    console.log(timeDate);
-    if (timeDate < 9) {
-        $(".body").css('background-image', 'url("images/barcelona-city-background-1.jpg")');
+    var weather = list[0].weather[0].main;
+    var imageUrl = "";
+    if (weather == "Rain") {
+        imageUrl = "rain.jpg";
     }
-    if (timeDate > 9) {
-        $(".body").css('background-image', 'url("images/barcelona-night-background.jpg")');
+    if (weather == "Clouds") {
+        imageUrl = "cloud.jpg"
     }
+    if (weather == "Snow") {
+        imageUrl = "snow.jpg";
+    }
+    if (weather == "Clear") {
+        imageUrl = "sun.jpg";
+    }
+    $(".body").css('background-image', 'url(/./style/images/' + imageUrl + ')');
+}
+
+function createIcon(weather) {
+    var icon = document.createElement("i");
+    if (weather == "Rain") {
+        $(icon).addClass("wi wi-rain")
+    }
+    if (weather == "Clouds") {
+        $(icon).addClass("wi wi-cloud")
+    }
+    if (weather == "Snow") {
+        $(icon).addClass("wi wi-snow")
+    }
+    if (weather == "Clear") {
+        $(icon).addClass("wi wi-day-sunny")
+    }
+    return icon;
 }
